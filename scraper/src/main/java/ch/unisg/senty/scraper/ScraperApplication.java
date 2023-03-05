9eec03240b30fa283bb202593a7b78e1848ea8c9
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2023-03-05 20:45:41 +0100

    no clue why this got added

--- scraper/src/main/java/ch/unisg/senty/scraper/ScraperApplication.java
+++ scraper/src/main/java/ch/unisg/senty/scraper/ScraperApplication.java
@@ -1,4 +1,4 @@
-package io.flowing.retail.inventory;
+package ch.unisg.senty.scraper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

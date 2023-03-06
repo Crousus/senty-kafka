17c1f4a4004d03b98962d3e936d5af78e34d805e
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2023-03-06 18:29:26 +0100

    modified consumer to count events to trigger email-notifier

--- comment-stream-consumer/src/main/java/ch.unisg.senty.commentstreamconsumer/application/CommentConsumer.java
+++ comment-stream-consumer/src/main/java/ch.unisg.senty.commentstreamconsumer/application/CommentConsumer.java
@@ -1,7 +1,5 @@
 package ch.unisg.senty.commentstreamconsumer.examples;
 
-
-
 import com.google.common.io.Resources;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -14,7 +12,6 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
-import static org.testng.Assert.assertEquals;
 
 public class ConsumerForAllEvents {
     public static void main(String[] args) throws IOException, ParseException {
@@ -30,6 +27,8 @@ public class ConsumerForAllEvents {
         // subscribe to relevant topics
         consumer.subscribe(Arrays.asList("gaze-events","click-events","senty"));
 
+        int counterSenty = 0;
+        int counterSentyThreshold = 20;
 
         while (true) {
 
@@ -42,17 +41,15 @@ public class ConsumerForAllEvents {
                 switch (record.topic()) {
                     //note: record.value() is a linkedHashMap (see utils.JavaDeserializer), use can use the following syntax to access specific attributes ((LinkedHashMap) record.value()).get("ATTRIBUTENAME").toString(); The object can be also reconstructed as Gaze object
                     case "senty":
-                        String value2 =   record.value().toString();
-                        System.out.println("Received senty - key: " + record.key() +"- value: " + value2 + "- partition: "+record.partition());
-                        break;
-
-                    case "gaze-events":
                         String value =   record.value().toString();
-                        System.out.println("Received gaze-events - key: " + record.key() +"- value: " + value + "- partition: "+record.partition());
-                        break;
 
-                    case "click-events":
-                        System.out.println("Received click-events - value: " + record.value()+ "- partition: "+record.partition());
+                        counterSenty = counterSenty + 1;
+                        if (counterSenty % counterSentyThreshold == 0) {
+                            System.out.println("Reached " + counterSenty + " senty events");
+                            System.out.println("--> Send Email \n");
+                        }
+                        // System.out.println("Received senty - key: " +
+                        // record.key() +"- value: " + value2 + "- partition: "+record.partition());
                         break;
 
                     default:

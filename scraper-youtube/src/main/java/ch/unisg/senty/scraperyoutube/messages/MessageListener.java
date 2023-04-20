5433adfb70fa9973aae8c9f61127e9c50a5e1074
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2023-04-20 04:54:47 +0200

    updated scraper-youtube to make api request etc

--- scraper-youtube/src/main/java/ch/unisg/senty/scraperyoutube/messages/MessageListener.java
+++ scraper-youtube/src/main/java/ch/unisg/senty/scraperyoutube/messages/MessageListener.java
@@ -10,8 +10,25 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import java.util.HashMap;
+import java.util.Map;
+
+
 @Component
 public class MessageListener {
+    @Value("${API-KEY}")
+    private String apiKey;
+
     @Autowired
     private ScraperService scraperService;
 
@@ -22,7 +39,7 @@ public class MessageListener {
     private MessageSender messageSender;
 
     @KafkaListener(id = "scraper-youtube", topics = MessageSender.TOPIC_NAME)
-    public void goodsFetchedEventReceived(String messageJson, @Header("type") String messageType) throws Exception {
+    public void messageReceived(String messageJson, @Header("type") String messageType) throws Exception {
         System.out.println("Received message: " + messageJson);
 
         if ("PingYouTubeScraperCommand".equals(messageType)) {
@@ -37,13 +54,48 @@ public class MessageListener {
         }
 
         if ("VerifyUrlCommand".equals(messageType)) {
+            JsonNode jsonNode = objectMapper.readTree(messageJson);
+            String url = jsonNode.get("data").asText();
 
-            Message<VerifyUrlCommandPayload> message = objectMapper.readValue(messagePayloadJson, new TypeReference<Message<FetchGoodsCommandPayload>>() {});
+            // e.g., https://www.youtube.com/watch?v=s_Nbg1tdDUA
+            // e.g., https://youtu.be/s_Nbg1tdDUA
+            Pattern pattern = Pattern.compile("(?<=v=|/videos/|/embed/|youtu\\.be/|/v/|/e/)[^#&?\\n]*");
+            Matcher matcher = pattern.matcher(url);
 
-            FetchGoodsCommandPayload fetchGoodsCommand = message.getData();
-            String pickId = inventoryService.pickItems( //
-                    fetchGoodsCommand.getItems(), fetchGoodsCommand.getReason(), fetchGoodsCommand.getRefId());
+            if (matcher.find()) {
+                // parse video ID
+                String videoId = matcher.group();
+                // System.out.println("Video ID: " + videoId);
 
+                // fetch video data
+                String videoData = fetchVideoData(videoId, apiKey);
+                // System.out.println("Video Data: " + videoData);
+
+                // check if length of "items" is 0
+                JsonNode rootNode = objectMapper.readTree(videoData);
+                JsonNode items = rootNode.get("items");
+                if (items.size() == 0) {
+                    // System.out.println("Video ID not found");
+                    // send event (without payload)
+                    Message<String> message = new Message<String>("UrlVerifiedEvent");
+                    messageSender.send(message);
+                    return;
+                }
+
+                Map<String, String> filteredData = filterVideoData(videoData);
+                // System.out.println("Filtered Data: " + filteredData);
+
+                // TODO: change to "UrlVerificationSucceededEvent" and "UrlVerificationFailedEvent"
+                // send event
+                Message<Map<String, String>> message = new Message<Map<String, String>>("UrlVerifiedEvent", filteredData);
+                messageSender.send(message);
+
+            } else {
+                // System.out.println("Could not parse video ID");
+                // send event (without payload)
+                Message<String> message = new Message<String>("UrlVerifiedEvent");
+                messageSender.send(message);
+            }
         }
 
         if ("TopUpTokensCommand".equals(messageType)) {
@@ -51,4 +103,47 @@ public class MessageListener {
         }
     }
 
+    private String fetchVideoData(String videoId, String apiKey) {
+        WebClient webClient = WebClient.create();
+        String url = "https://www.googleapis.com/youtube/v3/videos?id=" + videoId + "&part=snippet,statistics&key=" + apiKey;
+
+        Mono<String> response = webClient.get()
+                .uri(url)
+                .header("Accept", "application/json")
+                .retrieve()
+                .bodyToMono(String.class);
+
+        return response.block();
+    }
+
+    private Map<String, String> filterVideoData(String videoData) throws IOException {
+        // Deserialize JSON response
+        JsonNode rootNode = objectMapper.readTree(videoData);
+
+        // Extract desired fields
+        JsonNode videoNode = rootNode.path("items").get(0);
+        JsonNode snippetNode = videoNode.path("snippet");
+        JsonNode statisticsNode = videoNode.path("statistics");
+
+        String publishedAt = snippetNode.path("publishedAt").asText();
+        String title = snippetNode.path("title").asText();
+        String defaultThumbnailUrl = snippetNode.path("thumbnails").path("default").path("url").asText();
+        String channelTitle = snippetNode.path("channelTitle").asText();
+
+        String viewCount = statisticsNode.path("viewCount").asText();
+        String likeCount = statisticsNode.path("likeCount").asText();
+        String commentCount = statisticsNode.path("commentCount").asText();
+
+        // Create a new HashMap and insert extracted fields
+        Map<String, String> filteredData = new HashMap<>();
+        filteredData.put("title", title);
+        filteredData.put("channelTitle", channelTitle);
+        filteredData.put("publishedAt", publishedAt);
+        filteredData.put("defaultThumbnailUrl", defaultThumbnailUrl);
+        filteredData.put("viewCount", viewCount);
+        filteredData.put("likeCount", likeCount);
+        filteredData.put("commentCount", commentCount);
+
+        return filteredData;
+    }
 }
--- scraper-youtube/src/main/java/ch/unisg/senty/scraperyoutube/rest/ScraperRestController.java
+++ scraper-youtube/src/main/java/ch/unisg/senty/scraperyoutube/rest/ScraperRestController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,12 +17,22 @@ public class ScraperRestController {
   @Autowired
   private MessageSender messageSender;
   
-  @PostMapping(path = "/api/scraperyoutube/ping")
-  public ResponseEntity<String> placeOrder() {
+  @GetMapping(path = "/api/scraperyoutube/ping")
+  public ResponseEntity<String> ping() {
     Message message = new Message("PingYouTubeScraperCommand");
 
     messageSender.send(message);
 
+    String responseJson = "{\"traceId\": \"" + message.getTraceid() + "\"}";
+    return ResponseEntity.status(HttpStatus.OK).body(responseJson);
+  }
+
+  @GetMapping(path = "/api/scraperyoutube/verify")
+  public ResponseEntity<String> verify(@RequestParam String url) {
+    Message<String> message = new Message<String>("VerifyUrlCommand", url);
+
+    messageSender.send(message);
+    
     String responseJson = "{\"traceId\": \"" + message.getTraceid() + "\"}";
     return ResponseEntity.status(HttpStatus.OK).body(responseJson);
   }
